8e34f5b6f9268878c988977c71b3af81413a3135
murat birol <muriyu8@hotmail.com>
2018-10-17 18:33:55 +0300

    Add project files.

---
+++ DataAccess/StrategyPatterm/MyStrategy.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.StrategyPatterm
+{
+    public class MyStrategy: DropCreateDatabaseIfModelChanges<CelestialEntities>
+    {
+        protected override void Seed(CelestialEntities context)
+        {
+            //base.Seed(context);
+            //Model değişirse silip yeniden yaratmasını sağlayan sınıfımız DropCreateDatabaseIfModelChanges sınıfıdır. Bu sınıf sizden bir generic tip ister. Bu da hangi context sınıfı ile tanımlama yaptıysanız o contexti vermek zorundasınız.
+        }
+    }
+}
---
+++ Model/Entities/Log.cs
@@ -0,0 +1,20 @@
+﻿using Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entities
+{
+    public class Log : EntityBase, IDatabase
+    {
+        [Required]
+        public int ID { get; set; }
+        public User User { get; set; }
+        public LogAction Action { get; set; }
+        public string Description { get; set; }
+
+    }
+}
